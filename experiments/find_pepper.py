16d7f8a7bdee0bf13bd639a30bb3d257b6d8021f
Dan Konshin <konshin104@gmail.com>
2021-12-03 01:02:46 +0500

    rename file

--- experiments/find_pepper.py
+++ experiments/find_pepper.py
@@ -1,74 +1,113 @@
+import time
+
 import cv2
 import cv2 as cv
 import imutils
 import numpy as np
+from icecream import ic
 
-cv2.namedWindow("result")
-cv2.namedWindow("w1")
-img = cv2.imread('grechka.jpg')
-(R, G, B) = cv2.split(img)
-zeros = np.zeros(img.shape[:2], dtype="uint8")
-img = cv2.merge([zeros, zeros, R])
-
-img = imutils.resize(img, width=500)
-img = cv2.medianBlur(img, 5)
-hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-
-cv2.imshow('w1', hsv)
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (0, 0, 255)
+GREEN = (0, 255, 0)
+BLUE = (255, 0, 0)
 
 
-# gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-# ret, thresh = cv2.threshold(img, 50, 200, cv2.THRESH_BINARY)  # <--- Try different values here
-h_min = np.array((0, 0, 0), np.uint8)
-h_max = np.array((255, 222, 76), np.uint8)
-
-# накладываем фильтр на кадр в модели HSV
-thresh = cv2.inRange(hsv, h_min, h_max)
-cv2.imshow('w1', thresh)
-
-accum_size = 1
-# Minimum distance between the centers of the detected circles.
-minDist = 30
-# First method-specific parameter. In case of CV_HOUGH_GRADIENT , it is the higher threshold of the two passed to the Canny() edge detector (the lower one is twice smaller).
-param1 = 50
-# Second method-specific parameter. In case of CV_HOUGH_GRADIENT , it is the accumulator threshold for the circle centers at the detection stage. The smaller it is, the more false circles may be detected. Circles, corresponding to the larger accumulator values, will be returned first.
-param2 = 9
-#
-minRadius = 10
-#
-maxRadius = 30
+def thresh(img):
+    _, thresh = cv.threshold(img, 60, 255, cv.THRESH_BINARY)  # <--- Try different values here
+    return thresh
 
 
-def trash(img):
-    cimg = img
-    # circles = cv2.HoughCircles(
-    #     thresh,
-    #     cv2.HOUGH_GRADIENT,
-    #     accum_size,
-    #     minDist,
-    #     param1=param1,
-    #     param2=param2,
-    #     minRadius=minRadius,
-    #     maxRadius=maxRadius,
-    # )
-    # # circles = cv.HoughCircles(
-    # #     img, cv.HOUGH_GRADIENT, 1, 20, param1=50, param2=30, minRadius=0, maxRadius=0
-    # # )
-    # circles = np.uint16(np.around(circles))
-    # for i in circles[0, :]:
-    #     # draw the outer circle
-    #     cv.circle(cimg, (i[0], i[1]), i[2], (0, 255, 0), 2)
-    #     # draw the center of the circle
-    #     cv.circle(cimg, (i[0], i[1]), 2, (0, 0, 255), 3)
+def find_circles(img):
+    accum_size = 1
+    # Minimum distance between the centers of the detected circles.
+    minDist = 60
+    # First method-specific parameter. In case of CV_HOUGH_GRADIENT , it is the higher threshold of the two passed to the Canny() edge detector (the lower one is twice smaller).
+    param1 = 50
+    # Second method-specific parameter. In case of CV_HOUGH_GRADIENT , it is the accumulator threshold for the circle centers at the detection stage. The smaller it is, the more false circles may be detected. Circles, corresponding to the larger accumulator values, will be returned first.
+    param2 = 6
+    #
+    minRadius = 15
+    #
+    maxRadius = 30
+    cimg = cv.cvtColor(img, cv.COLOR_GRAY2BGR)
+    circles = cv.HoughCircles(
+        img,
+        cv.HOUGH_GRADIENT,
+        accum_size,
+        minDist,
+        param1=param1,
+        param2=param2,
+        minRadius=minRadius,
+        maxRadius=maxRadius,
+    )
+
+    circles = np.uint16(np.around(circles))
+    for i in circles[0, :]:
+        circle_img = np.zeros((img.shape[0], img.shape[1]), np.uint8)
+        cv2.circle(circle_img, (i[0], i[1]), i[2], WHITE, -1)
+        datos_rgb = cv2.mean(img, mask=circle_img)[::-1]
+        ic(datos_rgb)
+        if datos_rgb[3] < 70:
+            cv.circle(cimg, (i[0], i[1]), i[2], BLUE, 2)
+            # draw the center of the circle
+            cv.circle(cimg, (i[0], i[1]), 2, GREEN, 3)
     return cimg
 
 
-res = trash(img)
-cv2.imshow('result', res)
+def create_mask(img):
+    hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
+    lower = np.array([000, 33, 114])
+    upper = np.array([128, 255, 255])
+    mask = cv.inRange(hsv, lower, upper)
+    return mask
 
 
-while True:
-    ch = cv2.waitKey(5)
-    if ch == 27:
-        break
-cv2.destroyAllWindows()
+def wait_esc():
+    while True:
+        ch = cv.waitKey(5)
+        if ch == 27:
+            break
+
+
+def show_image(img):
+    cv2.namedWindow('Display', cv2.WINDOW_NORMAL)
+    cv2.imshow('Display', img)
+    wait_esc()
+    cv.destroyWindow('Display')
+
+
+def find_greatest_contour(contours):
+    largest_area = 0
+    largest_contour_index = -1
+    i = 0
+    total_contours = len(contours)
+    while i < total_contours:
+        area = cv2.contourArea(contours[i])
+        if area > largest_area:
+            largest_area = area
+            largest_contour_index = i
+        i += 1
+
+    return largest_area, largest_contour_index
+
+
+if __name__ == '__main__':
+
+    img = cv.imread('grechka.jpg')
+    img = imutils.resize(img, width=500)
+    img = cv2.GaussianBlur(img, (5, 5), 0)
+    img = cv.medianBlur(img, 5)
+    img = cv2.GaussianBlur(img, (5, 5), 0)
+    mask = create_mask(img)
+    # cv.imshow('mask', mask)
+
+    thr = thresh(mask)
+
+    contours, hierarchy = cv2.findContours(thr.astype(np.uint8), 1, 2)
+    cv2.drawContours(thr, contours, -1, (255, 255, 255), thickness=8)
+    show_image(thr)
+    img = find_circles(thr)
+    show_image(img)
+
+    cv.destroyAllWindows()
