dc60d69927a683b28b6eaa701bf11fe0e5df47a7
Your Name <you@example.com>
2021-12-03 16:10:15 +0000

    temp flask app

--- flask-app/app.py
+++ flask-app/app.py
@@ -12,10 +12,11 @@ import numpy as np
 from flask import Flask, Response, render_template, request, send_from_directory
 from flask_socketio import SocketIO
 from imutils.video import VideoStream
+from find_pepper import find_pepper
 
 app = Flask(__name__, static_folder="./assets", template_folder='./assets')
 socketio = SocketIO(app)
-
+cap = None
 
 @app.route('/')
 def index():
@@ -24,17 +25,23 @@ def index():
 
 @socketio.on('check')
 def gen(json):
-    cap = cv2.VideoCapture(0)
+    global cap
+    if cap is None:
+        cap = cv2.VideoCapture(-1)
     while cap.isOpened():
         ret, img = cap.read()
         if ret:
-            img = cv2.resize(img, (0, 0), fx=0.5, fy=0.5)
-            frame = cv2.imencode('.jpg', img)[1].tobytes()
+           # img = cv2.resize(img, (0, 0), fx=0.5, fy=0.5)
+            frame = find_pepper(img)
+            frame = cv2.imencode('.jpg', frame)[1].tobytes()
             frame = base64.encodebytes(frame).decode("utf-8")
             socketio.emit('image', frame)
-            socketio.sleep(0.05)
+            socketio.sleep(1)
         else:
+            cap.release()
             break
+    cap.release()
+    cap = None
     print('exiting gen')
 
 
