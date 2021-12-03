0a3094329302c788cbbf5d8bab4892a51bfc5fdc
ekooo <ekonshin@micont.ru>
2021-12-03 17:38:15 +0500

    number recognition

--- flask-app/app.py
+++ flask-app/app.py
@@ -1,10 +1,7 @@
-import argparse
 import base64
-import datetime
 import json
-import os
-import threading
-import time
+import requests
+
 
 import cv2
 import imutils
@@ -13,15 +10,36 @@ from flask import Flask, Response, render_template, request, send_from_directory
 from flask_socketio import SocketIO
 from imutils.video import VideoStream
 from find_pepper import find_pepper
+from find_sn import get_sn_carriage
+
+SERVER_URL = 'http://95.181.198.19:8000'
 
 app = Flask(__name__, static_folder="./assets", template_folder='./assets')
 socketio = SocketIO(app)
 cap = None
 
 @app.route('/')
+def login():
+    return render_template('login.html')
+
+
+@app.route('/index')
 def index():
     return render_template('index.html')
 
+
+@app.route('/index', methods= ['post'])
+def indexpost():
+    if request.method == 'POST':
+        trainnum = request.form.get('trainnum')
+        cargoweight = request.form.get('cargoweight')
+        carrtype = request.form.get('carrtype')
+        carrnum = request.form.get('carrnum')
+    message = json.dumps({"trainnum": trainnum, "cargoweight": cargoweight, "carrtype": carrtype, "carrnum": carrnum})
+    requests.post(SERVER_URL, message)
+
+
+    
 
 @socketio.on('check')
 def gen(json):
@@ -36,7 +54,17 @@ def gen(json):
             frame = cv2.imencode('.jpg', frame)[1].tobytes()
             frame = base64.encodebytes(frame).decode("utf-8")
             socketio.emit('image', frame)
-            socketio.sleep(1)
+
+            num = get_sn_carriage(img)
+            if num:
+                print(num)
+                try:
+                    numn = int(num[0])
+                except ValueError:
+                    continue
+                print(numn)
+                socketio.emit('number', numn)
+            socketio.sleep(0.05)
         else:
             cap.release()
             break
@@ -45,9 +73,6 @@ def gen(json):
     print('exiting gen')
 
 
-@app.route('/login')
-def login():
-    return render_template('login.html')
 
 
 if __name__ == '__main__':
