6c03da2f076583916f88a5ce0d70e2b6523b1db4
Shavkunov <kshavkunov@webiomed.ai>
2021-12-03 13:38:04 +0500

    fix(): requirements

--- operations/operators/admin_forms.py
+++ operations/operators/admin_forms.py
@@ -7,18 +7,12 @@ from . import models
 
 
 class GroupPlateAdminForm(ModelForm):
-    image = forms.FileField(allow_empty_file=True, required=False)
-    image_title = forms.CharField(required=False, disabled=True)
-
     class Meta:
         model = models.TransportationCarriageLog
         fields = '__all__'
 
 
 class CargoQualityAdminForm(ModelForm):
-    image = forms.FileField(allow_empty_file=True, required=False)
-    image_title = forms.CharField(required=False, disabled=True)
-
     class Meta:
         model = models.CargoQualityAssessment
         fields = '__all__'
